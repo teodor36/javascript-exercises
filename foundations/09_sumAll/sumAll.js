5e730de872d23a58cb1271aaf4baea871137787e
teodor36 <teodor0605@proton.me>
2026-06-03 03:30:16 +0300

    Solved foundations/09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (typeof a !== "number" || typeof b !== "number") return "ERROR";
+  if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+  if (a < 0 || b < 0) return "ERROR";
 
+  let min;
+  let max;
+
+  if (a < b) {
+    min = a;
+    max = b;
+  } else {
+    min = b;
+    max = a;
+  }
+
+  let sum = 0;
+
+  while (min <= max) {
+    sum += min++;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
