e074a7b3a52e93fdd4a34457bb85ad6ff37d75e0
teodor36 <teodor0605@proton.me>
2026-06-09 23:55:10 +0300

    Solved foundations/13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const strippedStr = str
+    .split(" ")
+    .join("")
+    .replace(/[^\w\s]/g, "")
+    .toLowerCase();
+  return strippedStr === strippedStr.split("").reverse().join("");
 };
 
 // Do not edit below this line
