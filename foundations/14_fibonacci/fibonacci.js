0ccaaa225ac271b11620bfddb9453619b72c9565
teodor36 <teodor0605@proton.me>
2026-06-10 00:00:29 +0300

    Solved foundations/14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  if (+n === 0) return 0;
+  if (+n < 0) return "OOPS";
 
+  const seq = [1, 1];
+  while (seq.length + 1 <= +n) {
+    seq.push(seq.at(-1) + seq.at(-2));
+  }
+
+  return seq.at(-1);
 };
 
 // Do not edit below this line
